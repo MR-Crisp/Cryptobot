363c08000e20e8291b740d5726f02ed0a2aa2454
MR-Crisp <aka.amin2005@gmail.com>
2022-12-14 16:14:12 +0000

    Init

--- retry.py
+++ retry.py
@@ -1,11 +1,45 @@
 from Crypto.Cipher import AES
 from getpass import getpass
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import REST, TimeFrame , TimeFrameUnit
+import time
+from alpaca_trade_api.common import URL
+from alpaca_trade_api.stream import Stream
 
 def login():
+
     print("login")
 
 def signup():
-    print("signup")
+    username = input("username: ")
+    while True:
+        password = getpass("password: ")
+        reconfirm = input("Re-enter password: ")
+        if reconfirm == password:
+            break
+        else:
+            print("password doesn't match")
+    print(f'Hi there {username}, we just need your Key and s-Key')
+    key = input("Key: ")
+    skey = input("s-Key: ")
+
+
+
+def validate(key,skey):
+    try:
+        api = tradeapi.REST(
+            key_id= key,
+            secret_key=skey,
+            base_url='https://paper-api.alpaca.markets'
+            )
+        account = api.get_account()
+        if account.status == 'ACTIVE':
+            veri = True
+            print('verified')
+    except:
+        print('Error your Key or Secret Key are wrong')
+
+
 
     
 def encrypt(key,plaintext):
