f79c9d7c806c7601b071c6fa6c85493c9ae4a0bd
Aamir Amin <aamir@Kaizars-MacBook-Air.local>
2023-11-02 11:47:05 +0000

    update

--- model.py
+++ model.py
@@ -1,102 +1,95 @@
-#https://aroussi.com/post/python-yahoo-finance
-#USE ROLLING MEAN https://www.learndatasci.com/tutorials/python-finance-part-3-moving-average-trading-strategy/
+# https://aroussi.com/post/python-yahoo-finance
+# USE ROLLING MEAN https://www.learndatasci.com/tutorials/python-finance-part-3-moving-average-trading-strategy/
 
 
 from datetime import datetime
 from PyQt5.QtWidgets import *
-from PyQt5 import uic 
+from PyQt5 import uic
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import * 
-from PyQt5.QtCore import * 
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from tools import *
+
 
 class MyGUI(QMainWindow):
     def __init__(self):
-        
-
-        #loads the xml file
+        # loads the xml file
         super(MyGUI, self).__init__()
-        uic.loadUi("C:/Users/akaam/Documents/Coding/Projects/Crypto bot/gui.ui",self)
+        uic.loadUi("/Users/aamir/programming/crypto/gui.ui", self)
         self.show()
 
         # configs the date note: this is slighty inefficient but works
         self.StartDate_tab1 = self.findChild(QDateEdit, "StartDate")
-        
-        self.EndDate_tab1 = self.findChild(QDateEdit, "EndDate")
-        
-        self.startdate_tab1  = str(self.Dateformat(self.StartDate_tab1.date())) # convert to regular
-        self.enddate_tab1 = str(self.Dateformat(self.EndDate_tab1.date())) # convert to regular
 
-        
+        self.EndDate_tab1 = self.findChild(QDateEdit, "EndDate")
+
+        self.startdate_tab1 = str(
+            self.Dateformat(self.StartDate_tab1.date())
+        )  # convert to regular
+        self.enddate_tab1 = str(
+            self.Dateformat(self.EndDate_tab1.date())
+        )  # convert to regular
+
         self.StartDate_tab1.dateChanged.connect(self.DateChanged)
         self.EndDate_tab1.dateChanged.connect(self.DateChanged)
 
-        self.Stock_select_tab1 = self.findChild(QComboBox,"StockcomboBox") 
+        self.Stock_select_tab1 = self.findChild(QComboBox, "StockcomboBox")
 
         # does all of the slider configs
-        self.CapitalSlider_tab1 = self.findChild(QSlider,"horizontalSlider")
-        self.CapitalLabel_tab1 = self.findChild(QLabel,"CapitalLabel")
+        self.CapitalSlider_tab1 = self.findChild(QSlider, "horizontalSlider")
+        self.CapitalLabel_tab1 = self.findChild(QLabel, "CapitalLabel")
         self.CapitalSlider_tab1.setMinimum(1)
         self.CapitalSlider_tab1.setMaximum(10_000)
         self.CapitalSlider_tab1.setSingleStep(100)
 
-        #graph configs ---------------------------------------------------------------- need to be worked out
-        
-
-
+        # graph configs ---------------------------------------------------------------- need to be worked out
 
         # self.figure = plt.figure()
         # self.canvas = FigureCanvas(self.figure)
 
         self.CapitalSlider_tab1.valueChanged.connect(self.calcCapitalSlider)
 
-        #code for when button is pressed
+        # code for when button is pressed
         self.pushButton_tab1 = self.findChild(QPushButton, "pushButton_tab1")
         self.pushButton_tab1.clicked.connect(lambda: self.MainButtonClick())
-        
 
-
-
-
-
-
-        #code for the second tab
+        # code for the second tab
 
         self.StartDate_tab2 = self.findChild(QCalendarWidget, "StartDate")
         self.EndDate_tab2 = self.findChild(QCalendarWidget, "StartDate")
 
-
-
-        
-        
-
     def calcCapitalSlider(self):
-        value_tab1  = self.CapitalSlider_tab1.value()
+        value_tab1 = self.CapitalSlider_tab1.value()
         self.CapitalLabel_tab1.setText(str(value_tab1))
 
-
-    def Dateformat(self,unformatted):
+    def Dateformat(self, unformatted):
         day = str(unformatted.day())
         month = str(unformatted.month())
         year = str(unformatted.year())
-        formatted = year+"-"+month+"-"+day
+        formatted = year + "-" + month + "-" + day
         return formatted
 
     def MainButtonClick(self):
-        
-
-        bot =  averageCrossover(self.Stock_select_tab1.currentText(),self.startdate_tab1,self.enddate_tab1,4,20)
-        #bot = averageCrossover('AAPL',"2017-01-01","2017-09-30",4,20)
+        bot = averageCrossover(
+            self.Stock_select_tab1.currentText(),
+            self.startdate_tab1,
+            self.enddate_tab1,
+            4,
+            20,
+        )
+        # bot = averageCrossover('AAPL',"2017-01-01","2017-09-30",4,20)
         bot.calc_MA()
         bot.algo(float(self.CapitalSlider_tab1.value()))
-        self.OutputLabel_tab1 = self.findChild(QLabel,"OutputLabel")
+        self.OutputLabel_tab1 = self.findChild(QLabel, "OutputLabel")
         self.OutputLabel_tab1.setText(str(bot.profit))
-        
-    def DateChanged(self):
-        self.startdate_tab1  = str(self.Dateformat(self.StartDate_tab1.date())) # convert to regular
-        self.enddate_tab1 = str(self.Dateformat(self.EndDate_tab1.date())) # convert to regular
 
-        
+    def DateChanged(self):
+        self.startdate_tab1 = str(
+            self.Dateformat(self.StartDate_tab1.date())
+        )  # convert to regular
+        self.enddate_tab1 = str(
+            self.Dateformat(self.EndDate_tab1.date())
+        )  # convert to regular
 
 
 def main():
@@ -104,17 +97,9 @@ def main():
     window = MyGUI()
     app.exec_()
 
-    
 
-if __name__ == '__main__':
-     main()
-
+if __name__ == "__main__":
+    main()
 
 
-
-
-
-
-
-
-#bot = averageCrossover('AAPL',"2017-01-01","2017-09-30",4,20)
+# bot = averageCrossover('AAPL',"2017-01-01","2017-09-30",4,20)
--- tools.py
+++ tools.py
@@ -18,77 +18,77 @@ class averageCrossover:
         self.longWindow = longWindow
         self.__FMA = None
         self.__SMA = None
-        self.dateformat = '%Y-%m-%d'
+        self.dateformat = "%Y-%m-%d"
         self.capital = 0
         self.profit = 0
         self.amount = 0
-    
+
     def calc_MA(self):
-        close = pd.DataFrame(self.data.Close, columns=['Close'])
+        close = pd.DataFrame(self.data.Close, columns=["Close"])
         self.__FMA = close.rolling(self.shortWindow).mean()
         self.__SMA = close.rolling(self.longWindow).mean()
-    
+
     def algo(self, capital):
         close = pd.DataFrame(self.data.Close)
 
         self.capital = capital
         bought = False
         sell_comparison = self.__FMA.values < self.__SMA.values
-        sell_comparison_str = np.where(sell_comparison, 'True', 'False')
-        self.__FMA['sell_comparison'] = sell_comparison_str
+        sell_comparison_str = np.where(sell_comparison, "True", "False")
+        self.__FMA["sell_comparison"] = sell_comparison_str
 
         for i in range(self.longWindow, len(self.__FMA)):
-            if (self.__FMA.iloc[i]['sell_comparison'] == 'False') and (not bought):
+            if (self.__FMA.iloc[i]["sell_comparison"] == "False") and (not bought):
                 price = close.iloc[i].astype(float)
                 self.amount = int(self.capital / price)
                 self.capital -= self.amount * price
                 bought = True
-                
-            elif (self.__FMA.iloc[i]['sell_comparison'] == 'True') and bought:
+
+            elif (self.__FMA.iloc[i]["sell_comparison"] == "True") and bought:
                 price = close.iloc[i].astype(float)
                 self.capital += price.iloc[0] * self.amount
                 bought = False
                 self.amount = 0
-                
 
         self.profit = float(self.capital - capital)
 
+
 # Create an instance of the class and run the strategy
-#bot = averageCrossover('AAPL', "2017-01-01", "2017-09-30", 4, 20)
+# bot = averageCrossover('AAPL', "2017-01-01", "2017-09-30", 4, 20)
 
 
-#https://www.youtube.com/watch?v=Gdpaita5GcE link for calc pivot points
+# https://www.youtube.com/watch?v=Gdpaita5GcE link for calc pivot points
 
 
 class PivotPoints:
-
     def visualize_pivots(self):
-        ax = self.df['Close'].plot(label='Close')
+        ax = self.df["Close"].plot(label="Close")
         for pivot in self.pivots:
-            if pivot >= self.df['Close'].min() and pivot <= self.df['Close'].max():
-                plt.axhline(y=pivot, color='r', linestyle='--', label='Pivot')
-        plt.xlabel('Date')
-        plt.ylabel('Price')
-        plt.title('Close Prices and Pivot Points')
+            if pivot >= self.df["Close"].min() and pivot <= self.df["Close"].max():
+                plt.axhline(y=pivot, color="r", linestyle="--", label="Pivot")
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.title("Close Prices and Pivot Points")
         plt.legend()
         plt.show()
 
-
-    def __init__(self,start,end,thresh,period,capital,sl,tp):
-        self.symbol = 'AAPL'
-        yf.pdr_override() # activatees the yahoo workarounds
+    def __init__(self, start, end, thresh, period, capital, sl, tp):
+        self.symbol = "AAPL"
+        yf.pdr_override()  # activatees the yahoo workarounds
         self.start = start
         self.end = end
-        self.df = pdr.get_data_yahoo(self.symbol, self.start- relativedelta(months=period), self.start) # creates dataframe of the symbol
+        self.df = pdr.get_data_yahoo(
+            self.symbol, self.start - relativedelta(months=period), self.start
+        )  # creates dataframe of the symbol
         self.pivots = []
-        self.threshold = thresh # ------------------------------------------------------------------------- very important parameter, as it is used to clean the data
-        self.capital = capital 
+        self.threshold = thresh  # ------------------------------------------------------------------------- very important parameter, as it is used to clean the data
+        self.capital = capital
         self.amount = 0
-        self.sl = sl  #----------------------------------------------------------------
-        self.tp = tp#----------------------------------------------------------------
+        self.sl = sl  # ----------------------------------------------------------------
+        self.tp = tp  # ----------------------------------------------------------------
         self.buy_in = 0
         self.current_price = 0
-      
+
     def merge_list(self, a, thresh):
         merged = []
         for value in a:
@@ -96,55 +96,57 @@ class PivotPoints:
                 merged.append(value)
         return merged
 
-
-
-
     def calcPoints(self):
-        
         self.pivots = []
 
         for i in range(4, len(self.df) - 1):
-            recent_low_prices = self.df['Low'][i-4:i+1]
-            recent_high_prices = self.df['High'][i-4:i+1]
+            recent_low_prices = self.df["Low"][i - 4 : i + 1]
+            recent_high_prices = self.df["High"][i - 4 : i + 1]
             currentMin = min(recent_low_prices)
             currentMax = max(recent_high_prices)
-            
-            if currentMin == self.df['Low'][i] or currentMax == self.df['High'][i]:
-                self.pivots.append(self.df['Close'][i])
+
+            if currentMin == self.df["Low"][i] or currentMax == self.df["High"][i]:
+                self.pivots.append(self.df["Close"][i])
         self.pivots = self.merge_list(self.pivots, self.threshold)
-        
-
-
-
 
     def pivot_logic(self):
-        self.df = pdr.get_data_yahoo(self.symbol, self.start, self.end) # inits new df with the new relevant dates
-        init_price = self.df['Close'].iloc[0]   # gets the first line of the df
+        self.df = pdr.get_data_yahoo(
+            self.symbol, self.start, self.end
+        )  # inits new df with the new relevant dates
+        init_price = self.df["Close"].iloc[0]  # gets the first line of the df
 
-        bought = False # set the bought variable so that the for loop can be broken later
+        bought = (
+            False  # set the bought variable so that the for loop can be broken later
+        )
 
-        for index, row in self.df.iloc[1:].iterrows():   # starts iterating through the df starting from SECOND row (least complex way to do this)
-            self.current_price = row['Close']
-            if not bought and init_price < self.current_price: # only allows for buy logic to run if the price is going up (compares current price and 1 price before it)
-                buy_condition = self.compare_pivots(init_price, self.current_price, self.pivots)
+        for index, row in self.df.iloc[
+            1:
+        ].iterrows():  # starts iterating through the df starting from SECOND row (least complex way to do this)
+            self.current_price = row["Close"]
+            if (
+                not bought and init_price < self.current_price
+            ):  # only allows for buy logic to run if the price is going up (compares current price and 1 price before it)
+                buy_condition = self.compare_pivots(
+                    init_price, self.current_price, self.pivots
+                )
                 if buy_condition:
                     self.amount = int(self.capital / self.current_price)
                     self.capital -= self.amount * self.current_price
                     bought = True
-                    self.buy_in = self.current_price 
-
-
+                    self.buy_in = self.current_price
 
             # take profit/stop loss logic
             if bought:
-                percent = 1 + self.percent_change(self.buy_in, self.current_price) # we want the percentage, not the change used to calculate when the TP/SL should activate
+                percent = 1 + self.percent_change(
+                    self.buy_in, self.current_price
+                )  # we want the percentage, not the change used to calculate when the TP/SL should activate
 
-                if percent > self.tp: # take profit
+                if percent > self.tp:  # take profit
                     self.capital += self.current_price * self.amount
                     bought = False
                     self.amount = 0
 
-                elif percent < self.sl: # stop loss logic
+                elif percent < self.sl:  # stop loss logic
                     self.capital += self.current_price * self.amount
                     bought = False
                     self.amount = 0
@@ -153,24 +155,21 @@ class PivotPoints:
         self.capital += self.current_price * self.amount
         print("Final capital:", self.capital)
 
-
-
-
-
-                
-    def compare_pivots(self, init, current, pivot_list): # compares the WHOLE list to the two prices
-        for item in pivot_list: 
-            if init < item < current:  
+    def compare_pivots(
+        self, init, current, pivot_list
+    ):  # compares the WHOLE list to the two prices
+        for item in pivot_list:
+            if init < item < current:
                 return True
         return False
 
-    def percent_change(self,start_point, end_point):
+    def percent_change(self, start_point, end_point):
         # This is going to be a little long but bare with me here
-        return ((float(end_point) - start_point) / abs(start_point)) 
+        return (float(end_point) - start_point) / abs(start_point)
 
 
 # bot = PivotPoints(dt.datetime(2017,1,1),dt.datetime(2023,6,1),2,36,1000,0.95,1.1)
 # bot.calcPoints()
 # bot.pivot_logic()
 
-#bot.visualize_pivots()
+# bot.visualize_pivots()
